6b990502c12b7d9b09fa50ff9c997a8c88ea4dc2
Andy Li <andy@onthewings.net>
2014-06-01 06:37:56 +0800

    wrap each line into a div like github itself

--- main.py
+++ main.py
@@ -6,6 +6,22 @@ from pygments import highlight
 from pygments.lexers import PythonLexer, get_lexer_for_filename
 from pygments.formatters import HtmlFormatter
 
+class GithubLikeHtmlFormatter(HtmlFormatter):
+    def __init__(self, **options):
+        HtmlFormatter.__init__(self, **options)
+        self.lineidprefix = options.get('lineidprefix', 'line')
+
+    def wrap(self, source, outfile):
+        return self._wrap_code(HtmlFormatter.wrap(self, source, outfile))
+
+    def _wrap_code(self, source):
+        line = self.linenostart - 1
+        for i, t in source:
+            if i == 1:
+                line += 1
+                t = "<div class='line' id='{}{}'>{}</div>".format(self.lineidprefix, line, t)
+            yield i, t
+
 class MainPage(webapp2.RequestHandler):
     def get(self):
         jinja_environment = jinja2.Environment(loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
@@ -22,14 +38,14 @@ class MainPage(webapp2.RequestHandler):
                 src = req.read() if encoding is 0 else unicode(req.read(), encoding)
                 
                 format_config = dict()
-                if 'linenos' in self.request.GET:
-                    format_config['linenos'] = self.request.GET['linenos']
+                if 'lineidprefix' in self.request.GET:
+                    format_config['lineidprefix'] = self.request.GET['lineidprefix']
                 if 'cssclass' in self.request.GET:
                     format_config['cssclass'] = self.request.GET['cssclass']
                 
                 values['highlighted'] = highlight(src, 
                                                   get_lexer_for_filename(url), 
-                                                  HtmlFormatter(**format_config))
+                                                  GithubLikeHtmlFormatter(**format_config))
             except urllib2.URLError as e:
                 values['error'] = 'URLError: {0}'.format(str(e))
             except urllib2.HTTPError as e:
